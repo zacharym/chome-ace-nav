16bda241fa0ac48840865c0d7cf9ca1e5173d126
Zachary Markin <zach.markin@gamechanger.io>
2015-08-28 16:18:36 -0400

    C-f to cancel

--- ace.js
+++ ace.js
@@ -13,12 +13,10 @@ function addPage(){
     });
 }
 
-function isMatch(str, pattern){
-    if(str.indexOf(pattern) > -1){
-        return true;
-    } else {
-        return false;
-    }
+function removePage(){
+    $('a').each(function(idx, val){
+        $(this).removeClass('pulse');
+    });
 }
 
 function filterPage(keys){
@@ -36,6 +34,14 @@ function filterPage(keys){
     return valids;
 }
 
+function isMatch(str, pattern){
+    if(str.indexOf(pattern) > -1){
+        return true;
+    } else {
+        return false;
+    }
+}
+
 function followLink(valids){
     console.log("about to follow link");
     console.log("valids: ", valids);
@@ -44,40 +50,48 @@ function followLink(valids){
     }
 }
 
+function addPrompt(){
+    var promptDiv = document.createElement("div");
+    document.body.insertBefore(promptDiv, document.body.firstChild);
+    document.body.firstChild.innerHTML="<div id='user-prompt'></div>";
+    $('#user-prompt').addClass('user-prompt');
+}
+
+function hidePrompt(){
+    $('#user-prompt').html('');
+    $('#user-prompt').hide();
+}
+
 var listener = new window.keypress.Listener();
 
 listener.simple_combo("control j", function(){
-    links = [];
-    $('a').each(function(idx, val){
-        links.push({
-            "index": idx,
-            "target": val,
-            "title": $(this).html()
-        });
-    });
     addPage();
+    addPrompt();
     var keys = [];
     var valids = [];
+    listener.simple_combo("control f", function(){
+        hidePrompt();
+        removePage();
+        $(document).unbind("keydown");
+    });
     $(document).keydown(function(e) {
-        if (e.keyCode == 32) {
+        if (e.keyCode == 13) {
+            followLink(valids);
+            reset();
+        } else if (e.keyCode == 32) {
             e.preventDefault();
             keys.push(32);
-        }
-        else if (e.keyCode == 13) {
-            console.log('got an enter key');
-            console.log('current valids: ');
-            alert('valids[0]: ', valids[0]);
-            followLink(valids);
         } else if (e.keyCode == 46 || e.keyCode == 8) {
             e.preventDefault();
             keys.pop();
-            valids = filterPage(keys);
-            console.log("keys: ", keys);
         } else {
             keys.push(e.keyCode);
-            valids = filterPage(keys);
-            console.log("keys: ", keys);
         }
+        valids = filterPage(keys);
+        var currentChars = keysToString(keys);
+        console.log(currentChars);
+        $('#user-prompt').html(currentChars);
+        console.log("currentChars: ", currentChars);
     });
 });
 
